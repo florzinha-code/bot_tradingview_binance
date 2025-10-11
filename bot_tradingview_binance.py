524a599edb3e5a62c1fc6d599995666fb2c17e87
florzinha-code <florzinha@bsd.com.br>
2025-10-11 03:47:27 -0400

    Update bot_tradingview_binance.py

--- bot_tradingview_binance.py
+++ bot_tradingview_binance.py
@@ -32,3 +32,7 @@ def webhook():
     except Exception as e:
         print("⚠️ Erro no webhook:", e)
         return jsonify({'error': str(e)}), 500
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
